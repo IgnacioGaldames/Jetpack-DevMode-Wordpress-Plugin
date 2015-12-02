2cc7dc91c0ce1691c56db99b7af2abcf91be1013
Ignacio Galdames <caminantestelar@gmail.com>
2015-12-02 16:52:37 -0300

    Version 1.0
    Version 1.0

--- trunk/jetpack_devmode/jetpack_devmode.php
+++ trunk/jetpack_devmode/jetpack_devmode.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Jetpack Dev Mode
-Plugin URI: 
+Plugin URI:
 Description: Activar o desactivar el wordpress de jetpack
 Author: Ignacio Galdames
 Author URI: http://ignaciogaldames.tk
-Version: 0.1.1
+Version: 1.0.0
 */
 
 
