340902ab01d56c860bfb52dabda889f8054ca3f6
Ignacio Galdames <caminantestelar@gmail.com>
2016-01-25 20:46:40 -0300

    ignaciogaldames.com
    Sitio web de autor actualizado

--- trunk/jetpack_devmode/jetpack_devmode.php
+++ trunk/jetpack_devmode/jetpack_devmode.php
@@ -4,8 +4,8 @@ Plugin Name: Jetpack Dev Mode
 Plugin URI: https://wordpress.org/plugins/jetpack-dev-mode/
 Description: Turn Jetpack Development on or off again, simply by activating this plugin
 Author: Ignacio Galdames
-Author URI: http://ignaciogaldames.tk
-Version: 1.0.0
+Author URI: http://ignaciogaldames.com
+Version: 1.0.1
 */
 
 
